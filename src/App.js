33e245e8b900671d3752cd29fca32bd2bd24485d
mohamedadeel97 <mohammed.adel.abdelgwad@gmail.com>
2024-07-15 03:13:36 +0300

    fetch customer and transaction data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { fetchCustomers, fetchTransactions } from "./services/api";
+
 
 function App() {
+  const [customers, setCustomers] = useState([]);
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    fetchCustomers().then((response) => {
+      setCustomers(response.data);
+    });
+    fetchTransactions().then((response) => {
+      setTransactions(response.data);
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="text-center mb-4">Customer Transactions</h1>
+
+
+      <table className="table table-responsive">
+        <thead className="thead-dark">
+          <tr>
+            <th>Name</th>
+            <th>Transactions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.map((customer) => {
+            return (
+              <tr
+                key={customer.id}
+              >
+                <td>{customer.name}</td>
+                <td>
+                  <div className="transaction-container">
+                    {transactions.map((tx) => (
+                      <div
+                        key={tx.id}
+
+                      >
+                        {tx.date} - ${tx.amount}
+                      </div>
+                    ))}
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
     </div>
   );
 }
